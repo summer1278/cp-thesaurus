2559bea45a53c350190ad9e0a68833a51c6c389b
summer1278 <cxcui188@gmail.com>
2017-08-01 10:42:25 +0100

    test

--- src/top_instances.py
+++ src/top_instances.py
@@ -257,28 +257,29 @@ def main():
     dict_name = sys.argv[1]
     # res_file.write("dataset, k, l2, true_instances, false_instances\n")
     # k=1000
-    kvals = [10,100,500,1000]
-    # kvals = [20]
+    # kvals = [10,100,500,1000]
+    kvals = [20]
     # datasets = ["TR"]
     dataset = 'TR'
 
     # datasets = ["TR", "CR", "SUBJ","MR", "B-D", "B-E", "B-K", "D-B", "D-E", "D-K", "E-B", "E-D", "E-K", "K-B", "K-D", "K-E"]
     
-    acc_file = open('../work/%s-%s-projection-acc'%(dict_name,dataset),'w')
-    # acc_file = open('../work/%s-%s-proposed-acc'%(dict_name,dataset),'w')
+    # acc_file = open('../work/%s-%s-projection-acc'%(dict_name,dataset),'w')
+    acc_file = open('../work/%s-%s-proposed-acc'%(dict_name,dataset),'w')
     # for dataset in datasets:
     for k in kvals:
         CP = expand.CP_EXPANDER()
         CP.load_CP_Dictionary("../data/%s" % dict_name, k)
-        # fname = "../work/%s-%s-proposed-%d" % (dataset,dict_name,k)
-        # res_file = open("%s-words"%fname, 'w')
-        # test_acc = batch_expansion(CP, res_file, dataset, k, fname)
-        # res_file.close()
-        
-        fname = "../work/%s-%s-projection-%d" % (dataset,dict_name,k)
+        fname = "../work/%s-%s-proposed-%d" % (dataset,dict_name,k)
         res_file = open("%s-words"%fname, 'w')
-        test_acc=batch_projection(CP, res_file, dataset, k, fname)
+        test_acc = batch_expansion(CP, res_file, dataset, k, fname)
         res_file.close()
+        
+        # fname = "../work/%s-%s-projection-%d" % (dataset,dict_name,k)
+        # res_file = open("%s-words"%fname, 'w')
+        # test_acc=batch_projection(CP, res_file, dataset, k, fname)
+        # res_file.close()
+        print test_acc
         acc_file.write('%f,%f\n'%(k, test_acc))
     acc_file.close()
 
