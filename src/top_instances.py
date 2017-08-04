d7b07e9d9308610df3897a1b687cdc90c5af06a2
summer1278 <cxcui188@gmail.com>
2017-08-04 17:18:24 +0100

    update arg

--- src/top_instances.py
+++ src/top_instances.py
@@ -340,12 +340,12 @@ def main():
 def test():
     dict_name = sys.argv[1]
     k = 20
-    opt = 2
+    opt = int(sys.argv[2])
     dataset = 'TR'
     fname = "../work/%d/%s-%s-proposed-%d" % (opt,dataset,dict_name,k)
     link_sentences_with_words(fname)
     pass
 
 if __name__ == '__main__':
-    main()
-    # test()
+    # main()
+    test()
