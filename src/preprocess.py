516aa84101d722e4d3869dbfdedbb01c82966e5a
summer1278 <cxcui188@gmail.com>
2017-08-13 12:57:06 +0100

    compute_links

--- src/preprocess.py
+++ src/preprocess.py
@@ -6,19 +6,25 @@ August 2017
 """
 def compute_links():
     F = open("../work/bigram.sorted","r")
-    output = open("../work/bigram_links.dat","w")
-    output.write("i \t j \t wij\n")
+    G = open("../work/bigram_links.dat","w")
+    G.write("i \t j \t wij\n")
     for line in F:
         p = line.strip().split(' ')
         i = get_id(p[0])
         j = get_id(p[1])
         w = float(p[2])
-        output.write("%s \t %s \t %f\n"%(i, j, w))
+        G.write("%s \t %s \t %f\n"%(i, j, w))
+    F.close()
+    G.close()
     pass
 
 # get node id for a word in D
+# from word_ids pre-generated
 def get_id(word):
-    pass
+    F=open("../work/word_ids","r")
+    words = [line[:-1] for line in F]
+    word_id = words.index(word)
+    return word_id
 
 # generate from unigrams or bigrams?
 # firstly, try using bigrams
@@ -31,8 +37,17 @@ def word_ids_generator():
         word_j = p[1]
         words.append(word_i)
         words.append(word_j)
-    print len(set(words)),len(words)
+    F.close()
+    print "words loaded = ",len(set(words))
+    G = open("../work/word_ids","w")
+    # i = 0
+    for word in set(words):
+        # G.write('%d %s\n'%(i,word))
+        G.write('%s\n'%(word))
+        # i+=1
+    G.close()
     pass
 
 if __name__ == '__main__':
-    word_ids_generator()
+    # word_ids_generator()
+    compute_links()
