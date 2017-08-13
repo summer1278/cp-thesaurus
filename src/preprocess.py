a52f247d76a8ff4ec05f7eb1c8ef876003bca684
summer1278 <cxcui188@gmail.com>
2017-08-13 12:19:36 +0100

    word ids generator from bigrams

--- src/preprocess.py
+++ src/preprocess.py
@@ -5,8 +5,8 @@ Xia Cui
 August 2017
 """
 def compute_links():
-    F = open(".../work/bigram.sorted","r")
-    output = open(".../work/bigram_links.dat","w")
+    F = open("../work/bigram.sorted","r")
+    output = open("../work/bigram_links.dat","w")
     output.write("i \t j \t wij\n")
     for line in F:
         p = line.strip().split(' ')
@@ -17,6 +17,22 @@ def compute_links():
     pass
 
 # get node id for a word in D
-# generate from unigrams or bigrams?
 def get_id(word):
     pass
+
+# generate from unigrams or bigrams?
+# firstly, try using bigrams
+def word_ids_generator():
+    words = []
+    F = open("../work/bigram.sorted","r")
+    for line in F:
+        p = line.strip().split(' ')
+        word_i = p[0]
+        word_j = p[1]
+        words.append(word_i)
+        words.append(word_j)
+    print len(set(words)),len(words)
+    pass
+
+if __name__ == '__main__':
+    word_ids_generator()
