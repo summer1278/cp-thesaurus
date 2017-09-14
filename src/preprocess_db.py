4cd9012165162f32f1d8e6496a5369d4197e9aee
summer1278 <cxcui188@gmail.com>
2017-09-14 10:16:11 +0100

    update

--- src/preprocess_db.py
+++ src/preprocess_db.py
@@ -117,13 +117,13 @@ def compute_ppmi_coreness(domain,k):
 
     ppmi_dict={}
     for x in features:
-        if x_total.get(x,0)>0 and x_src.get(x,0) > 0 and x_tgt.get(x,0) > 0:
+        if x_total.get(x,0) > 0 and x_src.get(x,0) > 0 and x_tgt.get(x,0) > 0:
             # print x_total.get(x,0), x_src.get(x,0), src_reviews, total_reviews
             src_ppmi = ppmi(x_total.get(x,0), x_src.get(x,0), src_reviews, total_reviews) 
             tgt_ppmi = ppmi(x_total.get(x,0), x_tgt.get(x,0), tgt_reviews, total_reviews)
             ppmi_dict[x] = (src_ppmi-tgt_ppmi)**2
     L = ppmi_dict.items()
-    L.sort(lambda x, y: -1 if x[1] < y[1] else 1)
+    L.sort(lambda x, y: -1 if x[1] > y[1] else 1)
     top_feats = [x for (x,val) in L[:k]]
     print top_feats, len(top_feats)
 
