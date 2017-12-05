3290de5fe354756f3fbb73725a257a4d02256440
summer1278 <cxcui188@gmail.com>
2017-12-05 13:11:16 +0000

    update nonoverlap

--- src/runner.py
+++ src/runner.py
@@ -17,14 +17,14 @@ def compute_ppmi_coreness(domain):
 
 def runner_nonoverlap(domain):
     # run km (nonoverlap)
-    call('time ../kmcpp/./km ../data/ppmi_links.dat ../data/%s/ppmi_coreness.dat ../data/%s/result_ppmi_nonoverlap.dat 100 1 10'%(domain,domain), shell=True)
+    # call('time ../kmcpp/./km ../data/ppmi_links.dat ../data/%s/ppmi_coreness.dat ../data/%s/result_ppmi_nonoverlap.dat 100 1 10'%(domain,domain), shell=True)
     preprocess.convert_cp_nonoverlap(domain,'ppmi')
     expand.main(domain,'nonoverlap')
     pass
 
 def runner_overlap(domain):
     # run km_overlap
-    #call('time ../kmcpp/./km_overlap ../data/ppmi_links.dat ../data/%s/ppmi_coreness.dat ../data/%s/result_ppmi_overlap.dat 10'%(domain,domain), shell=True)
+    # call('time ../kmcpp/./km_overlap ../data/ppmi_links.dat ../data/%s/ppmi_coreness.dat ../data/%s/result_ppmi_overlap.dat 10'%(domain,domain), shell=True)
     preprocess.convert_cp_overlap(domain,'ppmi')
     expand.main(domain,'overlap')
     pass
