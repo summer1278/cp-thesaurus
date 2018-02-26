ca29e5bea1d78c24637bb25d132aa508993cd08d
summer1278 <cxcui188@gmail.com>
2018-02-26 01:10:23 +0000

    add count

---
+++ src/count.py
@@ -0,0 +1,30 @@
+"""
+Count CP
+
+Xia Cui
+02/2018
+"""
+import numpy,math
+
+def count_CP(CP_fname):
+    all_peris = []
+    with open(CP_fname) as CP_file:
+        core = 0
+        for line in CP_file:
+            p = line.strip().split()
+            core +=1
+            peris = sum([1 for x in p[2:]])
+            print "current = ",core,peris
+            all_peris.append(peris)
+    print CP_fname
+    print "all = ", core, all_peris
+    average = float(sum(all_peris))/float(core)
+    print "average = ", average
+    pass
+
+if __name__ == '__main__':
+    # dataset = "TR"
+    # CP_fname = "../data/%s/cpwords_ppmi_overlap1.dat" %dataset
+    # CP_fname = "../data/cp-overalp.ppmi"
+    CP_fname = "../data/cp-nonoveralp.ppmi"
+    count_CP(CP_fname)
